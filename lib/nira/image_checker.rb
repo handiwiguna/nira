b973c572055c78ff100cf3abac936f3e152be3ce
Handi Wiguna <handi@HandiWiguna.local>
2013-01-02 09:29:46 +0700

    rename method

--- lib/nira/image_checker.rb
+++ lib/nira/image_checker.rb
@@ -13,21 +13,21 @@ module Nira
 
     def result
       passed = true
-      passed &&= check_min_size(image.width, image.height) if image.have_size_tag? && !!min_size
+      passed &&= greater_than_minimal_size?(image.width, image.height) if image.have_size_tag? && min_size
 
       if eager_check
-        passed &&= check_image_existence
-        passed &&= check_min_size(*(image.meta.size || [0, 0])) if min_size
+        passed &&= image_exists?
+        passed &&= greater_than_minimal_size?(*(image.meta.size || [0, 0])) if min_size
       end
 
       passed ? image.url : nil
     end
 
-    def check_image_existence
+    def image_exists?
       !image.meta.size.nil?
     end
 
-    def check_min_size(width, height)
+    def greater_than_minimal_size?(width, height)
       (width >= min_width) && (height >= min_height)
     end
   end
